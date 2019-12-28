a627a1ae256dc9e76da45b7052ca68233bcae855
jmishev <53108707+jmishev@users.noreply.github.com>
2019-12-28 14:19:40 +0200

    "second commit"

--- accommodation/wsgi.py
+++ accommodation/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for accomodation project.
+WSGI config for accommodation project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accomodation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accommodation.settings')
 
 application = get_wsgi_application()
